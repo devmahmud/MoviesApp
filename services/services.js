5a8ffe40a8e12679bc3c723a5b4cb171e1c60f86
Mahmudul Alam <expelmahmud@gmail.com>
2021-09-06 00:20:04 +0600

    add: api services

---
+++ services/services.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const apiUrl = 'https://api.themoviedb.org/3';
+const apiKey = '2e89540085d682a31ab096142c61c45a';
+
+// Get popular movies
+export const getPopularMovies = async () => {
+  const res = await axios.get(`${apiUrl}/movie/popular?api_key=${apiKey}`);
+  return res.data.results;
+};
+
+// Get Upcoming movies
+export const getUpcomingMovies = async () => {
+  const res = await axios.get(`${apiUrl}/movie/upcoming?api_key=${apiKey}`);
+  return res.data.results;
+};
+
+// Get Popular TV
+export const getPopularTv = async () => {
+  const res = await axios.get(`${apiUrl}/tv/popular?api_key=${apiKey}`);
+  return res.data.results;
+};
