c1c78f5d2066eb91b11d8022547ade62942760df
Mahmudul Alam <expelmahmud@gmail.com>
2021-09-07 01:53:27 +0600

    add: genre

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,5 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import {ScrollView, Image, StyleSheet, Dimensions} from 'react-native';
+import {
+  ScrollView,
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ActivityIndicator,
+  Text,
+} from 'react-native';
 import {getMovieDetail} from '../services/services';
 
 const {height} = Dimensions.get('screen');
@@ -21,25 +29,60 @@ const Detail = ({route}) => {
   }, [movieId]);
 
   return (
-    <ScrollView>
-      <Image
-        style={styles.image}
-        resizeMode="cover"
-        source={
-          movieDetail?.poster_path
-            ? {
-                uri: `https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`,
-              }
-            : placeholderImage
-        }
-      />
-    </ScrollView>
+    <>
+      {loaded && (
+        <ScrollView>
+          <Image
+            style={styles.image}
+            resizeMode="cover"
+            source={
+              movieDetail?.poster_path
+                ? {
+                    uri: `https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`,
+                  }
+                : placeholderImage
+            }
+          />
+          <View style={styles.container}>
+            <Text style={styles.title}>{movieDetail?.title}</Text>
+            <View style={styles.genreContainer}>
+              {movieDetail?.genres?.map(genre => (
+                <Text key={genre.id} style={styles.genre}>
+                  {genre.name}
+                </Text>
+              ))}
+            </View>
+          </View>
+        </ScrollView>
+      )}
+      {!loaded && <ActivityIndicator size="large" />}
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   image: {
     height: height / 2.5,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  genreContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  genre: {
+    marginHorizontal: 10,
+    fontWeight: 'bold',
   },
 });
 
